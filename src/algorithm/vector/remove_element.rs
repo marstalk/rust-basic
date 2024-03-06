9c7efb5a7a52173ffbb83731545599f70a43c761
jiacheng@home <jiacheng524@live.cn>
2024-03-06 20:00:34 +0800

    remove duplicate by target

--- src/algorithm/vector/remove_element.rs
+++ src/algorithm/vector/remove_element.rs
@@ -9,16 +9,20 @@ Return k.
 
 pub struct Solution();
 impl Solution {
+    // delete the target element
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
         let mut idx = -1;
         for i in 0..nums.len() {
             if nums[i] == val {
+                // if it's target element, skip it
                 continue;
             }
+            // otherwise, put it to the next position
             idx += 1;
-            nums[idx as usize] = val;
+            nums[idx as usize] = nums[i];
         }
 
+        // return the length
         idx + 1
     }
 }
@@ -30,7 +34,9 @@ mod tests {
     #[test]
     fn test_remove_element() {
         //nums = [3,2,2,3], val = 3
-        assert_eq!(Solution::remove_element(&mut vec![3, 2, 2, 3], 3), 2);
+        let mut nums = vec![3, 2, 2, 3];
+        assert_eq!(Solution::remove_element(&mut nums, 3), 2);
+        assert_eq!(nums[0..2], vec![2, 2]);
 
         //nums = [0,1,2,2,3,0,4,2], val = 2
         assert_eq!(
