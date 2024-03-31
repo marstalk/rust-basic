0693094600093ec61ab45b682d2609832bcafca8
jiacheng@home <jiacheng524@live.cn>
2024-03-31 21:08:38 +0800

    tweak

--- src/algorithm/string/longest_common_prefix.rs
+++ src/algorithm/string/longest_common_prefix.rs
@@ -7,14 +7,13 @@ If there is no common prefix, return an empty string "".
 pub fn longest_common_prefix(strings: Vec<String>) -> String {
     let mut res = String::from("");
     let mut idx = 0 as usize;
-    'a: for char in strings[0].chars() {
+    for char in strings[0].chars() {
         for str in strings[1..strings.len()].iter() {
             if idx >= str.len() || str.chars().nth(idx).unwrap() != char {
-                break 'a;
+                return res;
             }
         }
         res.push(char);
-
         idx += 1;
     }
     res
