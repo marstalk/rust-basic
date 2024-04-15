653cfdf74a9aad8725c07c5e5f9d0055be3d38a8
jiacheng@home <jiacheng524@live.cn>
2024-04-15 19:55:44 +0800

    lowest common ancenstor in binary search tree

---
+++ src/algorithm/tree/ancestor_problems3.rs
@@ -0,0 +1,14 @@
+use crate::algorithm::tree::binary_tree::{BTree, Node};
+
+
+/**
+ * p and q must in the binary search tree.
+ */
+pub fn lowest_common_ancestor_bst(btree: &BTree, p: i32, q: i32) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::algorithm::tree::binary_tree::BTree;
+
+}
