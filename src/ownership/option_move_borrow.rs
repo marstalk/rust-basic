90a1106bfeb7a58249995be5c6a37715c3696e3e
jiacheng@home <jiacheng524@live.cn>
2024-04-15 16:39:59 +0800

    more comment

--- src/ownership/option_move_borrow.rs
+++ src/ownership/option_move_borrow.rs
@@ -20,17 +20,20 @@ mod tests {
 
         let mut node_ref = &mut node;
 
+        // 1. ref mut next: is mutable borrow, but if next is moved. comparing this:
+        // while let Some(next) = nodex_ref.next{} here is partial move.
         while let Some(ref mut next) = node_ref.next {
             if next.val == 2 {
+                // 2. take() is very important, which move the Value but keep the Option as None
                 node_ref.next = next.next.take();
             } else {
-                // below compile failed, because next is partially mutable borrow from node_ref, wo node_ref itself can't changed.
+                // 3. below compile failed, because next is partially mutable borrow from node_ref, so node_ref itself can't changed.
                 // node_ref = next.deref_mut();
                 node_ref = node_ref.next.as_mut().unwrap();
             }
         }
 
-        // no partial move. which means that the next is still valid.
+        // 4. no partial move. which means that the next is still valid.
         // the node.next.take() doesn't equals to `let a = node.next; let b = a.take();`
         let third = node.next.take();
         assert_eq!(third.is_some(), true);
